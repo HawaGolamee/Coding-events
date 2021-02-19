3320771da8465c5c594931d7896718189e522089
HawaGolamee <73973675+HawaGolamee@users.noreply.github.com>
2021-02-18 20:05:27 -0600

    first commit

---
+++ src/main/java/org/codingevents/codingevents/controllers/EventControllers.java
@@ -0,0 +1,51 @@
+package org.codingevents.codingevents.controllers;
+
+import org.codingevents.codingevents.data.EventData;
+import org.codingevents.codingevents.models.Event;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+@Controller
+@RequestMapping("events")
+public class EventControllers {
+
+    @GetMapping
+    public String firstMethod(Model model) {
+        model.addAttribute("events", EventData.getAll());
+        return "events/index";
+    }
+
+    @GetMapping("create")
+    public String renderCreateEventForm() {
+        return "events/create";
+    }
+
+    @PostMapping("create")
+    public String processCreateEvent(@ModelAttribute Event newEvent) {
+        EventData.add(newEvent);
+        return "redirect:";
+    }
+
+    @GetMapping("delete")
+    public String displayDeleteEventForm(Model model) {
+        model.addAttribute("title", "Delete Events");
+        model.addAttribute("events", EventData.getAll());
+        return "events/delete";
+    }
+
+    @PostMapping("delete")
+    public String processDeleteEvent(@RequestParam(required = false) int[] eventIds) {
+        if (eventIds != null) {
+            for (int id : eventIds) {
+                EventData.remove(id);
+            }
+        }
+        return "redirect:";
+
+    }
+}
+
+
